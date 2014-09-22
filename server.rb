f83a3dda9e84c5de5365ed6150e23af518530012
Jeremy Fox <foxjerem@gmail.com>
2014-09-22 22:59:17 +0100

    Displaying products

--- server.rb
+++ server.rb
@@ -2,8 +2,12 @@ require 'sinatra/base'
 Dir[File.join(__dir__, 'lib', '*.rb')].each {|file| require file }
 
 class ClothingEStore < Sinatra::Base
+
+  DATA_FILE = 'products.txt'
+  PRODUCTS = ProductLoader.products_from(DATA_FILE)
   
   get '/' do
+    @products = PRODUCTS
     erb :index
   end
 
